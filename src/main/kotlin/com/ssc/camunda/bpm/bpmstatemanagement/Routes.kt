710475970a6ab32877d806429e009dae0be40f72
Bunyawat Singchai <bunyawat.s@gmail.com>
2020-10-12 17:31:47 +0700

    start first webflux endpoint

---
+++ src/main/kotlin/com/ssc/camunda/bpm/bpmstatemanagement/Routes.kt
@@ -0,0 +1,14 @@
+package com.ssc.camunda.bpm.bpmstatemanagement
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.function.server.ServerResponse
+import org.springframework.web.reactive.function.server.router
+
+@Configuration
+class SimpleRoute {
+    @Bean
+    fun route() = router {
+        GET("/route") { _ -> ServerResponse.ok().bodyValue(arrayOf(1, 2, 3)) }
+    }
+}
